f443cbc63514bed0c515fc447cb6390c7a373356
LUCAS DE ARAUJO <53193423+Beninigmal@users.noreply.github.com>
2025-07-01 20:42:02 -0300

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,10 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 3000),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['key1', 'key2']),
   },
+  // Configuração crítica para serverless:
+  emitErrors: false,
+  url: env('PUBLIC_URL', 'https://meu-site-fotografia.up.railway.app'),
 });
